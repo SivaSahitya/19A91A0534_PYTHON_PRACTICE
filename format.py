01c542db84a06c1201284aae71b2877cd0925daa
SivaSahitya <82566266+SivaSahitya@users.noreply.github.com>
2021-05-23 19:51:49 +0530

    Create format.py

---
+++ format.py
@@ -0,0 +1,10 @@
+#implement python code to format the character ,hex and octal
+>>> a=12
+>>> print("no is %X"%(a))
+#no is C
+>>> ch='p'
+>>> print('the character is %c'%(ch))
+#the character is p
+>>> b=6
+>>> print('the octal number is %o'%(b))
+#the octal number is 6
